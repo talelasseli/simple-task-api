85e742f31d89df848aad86ea60e6104c224c949a
Amin Labidi <talelasseli@gmail.com>
2026-04-14 19:11:01 +0100

    test

--- src/modules/tasks/task.routes.ts
+++ src/modules/tasks/task.routes.ts
@@ -5,10 +5,10 @@ import { authMiddleware } from "../../middlewares/auth.middleware";
 const router = Router();
 
 router.use(authMiddleware);
-
+//new changeeeeeee
 router.get("/", controller.getTasks);
 router.post("/", controller.createTask);
 router.put("/:id", controller.updateTask);
 router.delete("/:id", controller.deleteTask);
-
+router.get("/:id", controller.getTask);
 export default router;
